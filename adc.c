67f6da574827edb655a813be3fb24f98148d17f1
Daniel Mittmann <d.mittmann@io-dev.de>
2022-11-04 15:08:26 +0100

    Analog to digital conversion

--- adc.c
+++ adc.c
@@ -2,11 +2,22 @@
 
 void adc_init (void)
 {
-
+    // FVRCON
+    FVRCON = 0b10000001;                // Gain 1x (1,024V)
+    while(!(FVRCON & 0x40)){}           // wait till stable
+    
+    // ADC
+    analog_port &=    (~analog_pin);
+    analog_tris |=      analog_pin;
+    ANSELA      |=      0x01;           // RA0 = Analog Pin
+    ADCON1      =       0b11100011;     // connected to FVR
+    ADCON0      =       0b00000000; 
+    ADCON0      |=      0x01;           // switch ADC on
 }
 
-void adc_read (void)
+unsigned int adc_read (void)
 {
-    
-    
+    ADCON0      |=      0x02;           // Start conversion
+    while (ADCON0 & 0x02){}
+    return ((unsigned int)((ADRESH << 8) + ADRESL));
 }
