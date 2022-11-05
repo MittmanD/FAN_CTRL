68df2e3239bed1ecb23a142ebdf26b74ed4ae5f7
Daniel Mittmann <daniel@mittmann.org>
2022-11-05 11:57:45 +0100

    PWM config

--- timer.c
+++ timer.c
@@ -3,24 +3,16 @@
 
 void timer2_init (void)
 {
-    OSCEN       |= MFOEN;           // Switch MFINTOSC on
-    while(!(OSCSTAT & MFOR)){}      // Wait till MFINTOSC is stable
-    T2CLKCON = 0b00000101;          // MFINTOSC (500 kHz)
-    T2HLT = 0x00;                   // Software gate mode
-    T2RST = 0x00;                   // T2INPPS (Source ??????)
-    T2PR = 0xFF;                    // Timer 2 period
-    T2TMR = 0x00;
-    PIE1    &= (~TMR2IE);           // Disable Timer 2 interrupt
+    OSCEN       |= MFOEN;               // Switch MFINTOSC on
+    while(!(OSCSTAT & MFOR)){}          // Wait till MFINTOSC is stable
+    T2CLKCON    = 0b00000001;           // HFINT/4 - must be this mode according to pwm spec
+    T2HLT       = 0x00;                 // Software gate mode
+    T2RST       = 0x00;                 // T2INPPS (Source ??????)
+    T2PR        = 0xFF;                 // Timer 2 period
+    T2TMR       = 0x00;
+    PIE1        &= (~TMR2IE);           // Disable Timer 2 interrupt
+    PIR1        &= (~TMR2IF);           // clear timer 2 interrupt flag
+    T2CON       = 0b01111111;           // 1:128, 1:16, timer off
+    T2CON       |= ON;                  // switch on timer 2
 }
 
-void timer2_start (void)
-{
-    
-    
-}
-
-void timer2_isr (void)
-{
-    
-    
-}
--- timer.h
+++ timer.h
@@ -4,3 +4,5 @@
 #define     MFOEN   0x20    // MF osc enable
 #define     MFOR    0x20    // MF osc ready
 #define     TMR2IE  0x40    // Timer 2 interrupt enable
+#define     TMR2IF  0x40    // Timer 2 interrupt flag
+#define     ON      0x80    // Timer 2 On
